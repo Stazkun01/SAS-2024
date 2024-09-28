eaad9dc9829931fd881e41c831848fc3f7999160
Stazkun01 <123759398+Stazkun01@users.noreply.github.com>
2024-09-28 10:32:36 +0200

    exercice3.c

--- mise en situation/exercice3.c
+++ mise en situation/exercice3.c
@@ -1,23 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-	int n;
-	printf("donnez un nombre entier\n");
-	scanf("%d" , &n);
-	if(n<=2){
-		printf("c'est un nombre premier\n");
-		return 0;
-	}
-	else{
-		int i;
-	    for(i=2;i<n;i++){
-	    	if(n%i==0){
-	    		printf("c'est pas un nombre premier\n");
-	    		return 0;
-			}
-	    	
-		}
-		
-	}
-	printf("c'est un nombre premier");
-	return 0;
+        int n;
+                printf(" entrer un nombre entier :");
+                scanf("%d" ,&n);
+    if(n <= 2){
+                printf("c'est un nombre premier\n");
+                return 0;
+ }
+    else {
+        int i;
+        for (i =2 ;i< n;i++){
+            if(n % i == 0){
+        printf("ce n'est pas un nombre premier\n");
+        return 0;}
+    }
+    }
+        printf("c'est un nombre premier\n");
+    return 0;
 }
